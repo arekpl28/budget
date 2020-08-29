40eb8ba09422cc29f33ea2414a6d154171d91a44
Arkadiusz Plumbaum <arekpl28@wp.pl>
2020-08-29 17:42:14 +0200

    Add budget and common reducer

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import App from "./App";
 import "./i18n/i18n";
+import configureStore from "data/store";
+
+const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
